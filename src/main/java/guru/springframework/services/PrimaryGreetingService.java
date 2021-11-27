33a455703474398e5f64f74d6fcca5e15680fe08
m-abdelkawy <engineer_m.abdelkawy@yahoo.com>
2021-11-26 23:49:19 -0600

    di assignment Correct errors

--- src/main/java/guru/springframework/services/PrimaryGreetingService.java
+++ src/main/java/guru/springframework/services/PrimaryGreetingService.java
@@ -14,6 +14,10 @@ public class PrimaryGreetingService implements GreetingService {
 
     private GreetingRepository greetingRepository;
 
+    PrimaryGreetingService(GreetingRepository greetingRepository){
+        this.greetingRepository = greetingRepository;
+    }
+
     @Override
     public String sayGreeting() {
         return greetingRepository.getEnglishGreeting();
